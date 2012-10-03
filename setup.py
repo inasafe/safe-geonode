0b888cf1e6f96887601f1afa4258d9975cdca190
Ariel Núñez <ingenieroariel@gmail.com>
2012-10-03 17:44:29 -0400

    Brought automated script to find packages and data

--- setup.py
+++ setup.py
@@ -1,8 +1,48 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-import os
-
 from distutils.core import setup
+from distutils.command.install import INSTALL_SCHEMES
+import os
+import sys
+
+def fullsplit(path, result=None):
+    """
+    Split a pathname into components (the opposite of os.path.join) in a
+    platform-neutral way.
+    """
+    if result is None:
+        result = []
+    head, tail = os.path.split(path)
+    if head == '':
+        return [tail] + result
+    if head == path:
+        return result
+    return fullsplit(head, [tail] + result)
+
+# Tell distutils not to put the data_files in platform-specific installation
+# locations. See here for an explanation:
+# http://groups.google.com/group/comp.lang.python/browse_thread/thread/35ec7b2fed36eaec/2105ee4d9e8042cb
+for scheme in INSTALL_SCHEMES.values():
+    scheme['data'] = scheme['purelib']
+
+# Compile the list of packages available, because distutils doesn't have
+# an easy way to do this.
+packages, data_files = [], []
+root_dir = os.path.dirname(__file__)
+if root_dir != '':
+    os.chdir(root_dir)
+geonode_dir = 'geonode'
+
+for dirpath, dirnames, filenames in os.walk(geonode_dir):
+    # Ignore dirnames that start with '.'
+    for i, dirname in enumerate(dirnames):
+        if dirname.startswith('.'): del dirnames[i]
+    if '__init__.py' in filenames:
+        packages.append('.'.join(fullsplit(dirpath)))
+    elif filenames:
+        data_files.append([dirpath, [os.path.join(dirpath, f) for f in filenames]])
+
+
 safe_geonode = __import__('safe_geonode')
 
 
@@ -27,19 +67,8 @@ setup(
         'django-leaflet>=0.2.0',    # pip install django-leaflet
         'pygments',                 # pip install pygments
     ],
-    packages = ['safe_geonode',
-                'safe_geonode.tests',
-                'safe_geonode.management',
-                'safe_geonode.management.commands',],
-    package_data = {'safe_geonode': [
-                'safe_geonode/templates/*',
-                'safe_geonode/templates/safe/*',
-                'safe_geonode/static/*',
-                'safe_geonode/static/safe/*',
-                'safe_geonode/static/safe/js',
-                'safe_geonode/static/safe/css',
-                'safe_geonode/static/safe/img',
-                ]},
+    packages = packages,
+    data_files=data_files,
     scripts = [],
     classifiers = [
    ],
